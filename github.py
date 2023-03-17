661e5a29a9a5b6afc650429472b20afd4d633353
Suvendu <suvendu.chok@gmail.com>
2023-03-17 14:32:30 +0530

    add github.py file

---
+++ github.py
@@ -0,0 +1,18 @@
+import os 
+
+def usecom(comd:str)->None:
+	os.system(comd)
+
+
+comd="git add ."
+usecom(comd)
+
+sen=input("Enter your commit msg : ")
+
+comd=f"git commit -m \"{sen}\""
+usecom(comd)
+
+
+comd="git push origin master"
+usecom(comd)
+
